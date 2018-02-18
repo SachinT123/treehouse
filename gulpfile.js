e757d1676b76299f4a23623fdda09d4de5b66950
Sachin <sachin.tibrewal@innoraft.com>
2018-02-18 21:08:53 +0530

    optimized images and minified css

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,16 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var cssMin = require('gulp-css');
 var uglify = require('gulp-uglify');
-gulp.task('default', function(){
-  console.log("gulp is running");
-})
 gulp.task('css',function(){
 
   gulp.src([
-    'stylesheets/combined_css.css',
-    'stylesheets/app.css'
+    './stylesheets/combined_css.css',
+    './stylesheets/app.css'
   ])
     .pipe(concat('app.css'))
     .pipe(cssMin())
-    .pipe(gulp.dest('stylesheets'))
+    .pipe(gulp.dest('./stylesheets'))
 
 });
+
+gulp.task('default', ['css']);
