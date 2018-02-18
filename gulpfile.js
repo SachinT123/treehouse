2baa31a0291272dc9e5098cdf66201968bb5d945
Sachin <sachin.tibrewal@innoraft.com>
2018-02-18 21:44:06 +0530

    minified html

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var cssMin = require('gulp-css');
 var uglify = require('gulp-uglify');
+var htmlmin = require('gulp-htmlmin');
 gulp.task('css',function(){
 
   gulp.src([
@@ -14,4 +15,16 @@ gulp.task('css',function(){
 
 });
 
-gulp.task('default', ['css']);
+
+gulp.task("htmlmin", function(){
+  gulp.src("./index.html")
+  .pipe(htmlmin({collapseWhitespace:true}))
+  .pipe(gulp.dest("minified"));
+
+
+
+});
+gulp.task('default', [
+  'css',
+  'htmlmin'
+]);
